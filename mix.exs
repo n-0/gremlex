a30eed777c998f1d1abf84b37bb09a05dc1300fe
Barak <itsbarakyo@gmail.com>
2018-03-24 09:52:35 -0700

    ci(mix): read version from file
    mix.exs now reads the version number from a file

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Gremlex.MixProject do
   def project do
     [
       app: :gremlex,
-      version: "0.1.0",
+      version: File.read!("./VERSION") |> String.trim(),
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
