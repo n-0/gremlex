c581abe23af6b1e923fc4b3f973825e33de16257
barakyo <itsbarakyo@gmail.com>
2019-05-21 20:11:21 -0700

    Added test for websocket closed case

--- test/client_test.exs
+++ test/client_test.exs
@@ -106,5 +106,13 @@ defmodule Gremlex.ClientTests do
       assert vertex.label == "person"
       assert vertex.properties == %{name: ["jasper"]}
     end
+
+    test "returns an error tuple for long queries" do
+      {:ok, query} = File.read("test/support/long_query.groovy")
+      {status, reason, msg} = Gremlex.Client.query(query)
+      assert(status == :error)
+      assert(reason == :websocket_closed)
+      assert(msg == nil)
+    end
   end
 end
